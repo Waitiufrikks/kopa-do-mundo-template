a5273d3ddc0dfa78751d2315275f9eacf728d4e1
Eduardo Henrique <eduardohairescampos@gmail.com>
2023-06-02 12:35:07 -0300

    fix: adiciona o sqlite no gitignore e exclui os remove dos anos 1942 e 1946

--- utils.py
+++ utils.py
@@ -5,8 +5,6 @@ from exceptions import NegativeTitlesError, InvalidYearCupError, ImpossibleTitle
 def data_processing(team):
     current_year = datetime.now().year
     total_years_cup = list(range(1930, current_year, 4))
-    total_years_cup.remove(1942)
-    total_years_cup.remove(1946)
 
     team_first_cup = int(team["first_cup"][:4])
 
@@ -18,3 +16,15 @@ def data_processing(team):
 
     if team["titles"] > len(total_years_cup) - total_years_cup.index(team_first_cup):
         raise ImpossibleTitlesError("impossible to have more titles than disputed cups")
+    print(total_years_cup)
+
+
+test = {
+    "name": "Brasil",
+    "titles": 5,
+    "top_scorer": "Pelé",
+    "fifa_code": "BRA",
+    "first_cup": "1930-08-18",
+}
+
+data_processing(test)
